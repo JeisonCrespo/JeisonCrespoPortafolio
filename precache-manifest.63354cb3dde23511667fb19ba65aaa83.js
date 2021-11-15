5070df1d9059275b4d70d519837a2045c9a4e21f
Jeison Crespo <jeisoncrespo14@gmail.com>
2021-11-15 17:44:55 -0500

    Updates

---
+++ precache-manifest.63354cb3dde23511667fb19ba65aaa83.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c9d2aaaf0536c6581f84c918810509",
+    "url": "/JeisonCrespoPortafolio/index.html"
+  },
+  {
+    "revision": "5b8d834a144f44620330",
+    "url": "/JeisonCrespoPortafolio/static/css/main.3b08025a.chunk.css"
+  },
+  {
+    "revision": "61186a8bcadb8a1a0998",
+    "url": "/JeisonCrespoPortafolio/static/js/2.c6dd9c94.chunk.js"
+  },
+  {
+    "revision": "ed961b1d682fddd39f72e5d7dd46c4a2",
+    "url": "/JeisonCrespoPortafolio/static/js/2.c6dd9c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8d834a144f44620330",
+    "url": "/JeisonCrespoPortafolio/static/js/main.38caba93.chunk.js"
+  },
+  {
+    "revision": "97ebf569c7cda930077f",
+    "url": "/JeisonCrespoPortafolio/static/js/runtime-main.ee654af0.js"
+  },
+  {
+    "revision": "29833e17e43e8a8c76f2ab9c6dd1bb34",
+    "url": "/JeisonCrespoPortafolio/static/media/app-store.29833e17.svg"
+  },
+  {
+    "revision": "6c16262b4149f39fc03d33e78bdca285",
+    "url": "/JeisonCrespoPortafolio/static/media/cancel.6c16262b.svg"
+  },
+  {
+    "revision": "d2021dc8fe7120e0d24b6e50ca87a9e9",
+    "url": "/JeisonCrespoPortafolio/static/media/cancelSearch.d2021dc8.svg"
+  },
+  {
+    "revision": "787457507f4acc3cdbc5d91bdfce7c0f",
+    "url": "/JeisonCrespoPortafolio/static/media/email.78745750.svg"
+  },
+  {
+    "revision": "3ee26ec27083832441b794e33881be78",
+    "url": "/JeisonCrespoPortafolio/static/media/flores.3ee26ec2.svg"
+  },
+  {
+    "revision": "06b7e22f6b4e888c0b24667ee3c244ff",
+    "url": "/JeisonCrespoPortafolio/static/media/github.06b7e22f.svg"
+  },
+  {
+    "revision": "4df4227a8833830fcd411329d111eaa3",
+    "url": "/JeisonCrespoPortafolio/static/media/google-play.4df4227a.svg"
+  },
+  {
+    "revision": "ced8ec57a12259d20006b14fcd7ff6c7",
+    "url": "/JeisonCrespoPortafolio/static/media/instagram.ced8ec57.svg"
+  },
+  {
+    "revision": "d0205110d3747b7b7863abcb6e963c25",
+    "url": "/JeisonCrespoPortafolio/static/media/jeisoncrespo.d0205110.png"
+  },
+  {
+    "revision": "81198e0a77d2c9a0cd2564d6588ed98a",
+    "url": "/JeisonCrespoPortafolio/static/media/link.81198e0a.svg"
+  },
+  {
+    "revision": "7ab0545026332137a8ac25a700a8a2b7",
+    "url": "/JeisonCrespoPortafolio/static/media/linkedin.7ab05450.svg"
+  },
+  {
+    "revision": "c6a21cea3e5b3ae2ae4a96f6e27a5905",
+    "url": "/JeisonCrespoPortafolio/static/media/logo.c6a21cea.png"
+  },
+  {
+    "revision": "17c35bdbc2cda707ae7bca0269294f6b",
+    "url": "/JeisonCrespoPortafolio/static/media/macbook.17c35bdb.png"
+  },
+  {
+    "revision": "643491ae8959e59ffe1f37795089aa4d",
+    "url": "/JeisonCrespoPortafolio/static/media/menu.643491ae.svg"
+  },
+  {
+    "revision": "e1f7f8f9e216752356ce2533c13941a6",
+    "url": "/JeisonCrespoPortafolio/static/media/search.e1f7f8f9.svg"
+  },
+  {
+    "revision": "3c3f5e1e0fc0b10eb30ebabcb6e32b9b",
+    "url": "/JeisonCrespoPortafolio/static/media/whatsapp.3c3f5e1e.svg"
+  },
+  {
+    "revision": "195d87a95f715f1a4b7b4bdc7190e6d2",
+    "url": "/JeisonCrespoPortafolio/static/media/youtube.195d87a9.svg"
+  }
+]);
